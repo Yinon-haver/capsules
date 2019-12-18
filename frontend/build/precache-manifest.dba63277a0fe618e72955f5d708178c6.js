64c9d3eb17e1ea04d113873608d421893366ca04
Aviv Cohen <avivcohen333@gmail.com>
2019-12-19 00:24:38 +0200

    frontend test fix bug

--- frontend/build/precache-manifest.dba63277a0fe618e72955f5d708178c6.js
+++ frontend/build/precache-manifest.dba63277a0fe618e72955f5d708178c6.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5ef6e2ae2dfcde0f19c9a426a3055087",
+    "revision": "6d99160afd7c7f36dc03ec09cec6df4d",
     "url": "/index.html"
   },
   {
-    "revision": "700270a6df9b13e991af",
+    "revision": "89794f122273e19c76be",
     "url": "/static/css/main.9fe13672.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.fa229c1e.chunk.js.LICENSE"
   },
   {
-    "revision": "700270a6df9b13e991af",
-    "url": "/static/js/main.b51cc832.chunk.js"
+    "revision": "89794f122273e19c76be",
+    "url": "/static/js/main.6eefde2d.chunk.js"
   },
   {
     "revision": "64d808e467393c708fcb",
