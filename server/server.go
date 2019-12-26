94e494cd440cd3b4fd2812a0207aaf23206c64f5
Aviv Cohen <avivcohen333@gmail.com>
2019-12-26 11:57:42 +0200

    add checking

--- server/server.go
+++ server/server.go
@@ -179,6 +179,9 @@ func openChatConnectionHandler(writer http.ResponseWriter, request *http.Request
 }
 
 func Run() {
+	logger.Info("_______________________ running _______________________")
+	logger.Warning("_______________________ running _______________________")
+
 	var fs http.Handler
 
 	if config.GetIsReleaseMode() {
